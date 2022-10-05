8d7c0012af40f1f1e1a387c73fb0798ec65c3314
Prashant Sahib <prashantsd97@gmail.com>
2022-10-05 16:53:17 +0530

    deploy config

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import '../src/assets/fonts/Poppins-Bold.ttf';
 function App() {
 
   return (
-    <Router basename= {process.env.PUBLIC_URL}>
+    <Router basename= {'/saatvikapps'}>
       <div className="App">
         <Routes>
           <Route path='/' element = {<Home />}/>
